3e4e8b132548c5af5354a67c326620b63a40a95c
Naoufal Kribet <naoufalkribet@gmail.com>
2025-10-10 14:38:35 +0200

    feat(linalg): Implement and test Vector class

--- bindings/geomath_bindings.cpp
+++ bindings/geomath_bindings.cpp
@@ -1,12 +1,24 @@
 #include <pybind11/pybind11.h>
-#include <geomath/Dummy.hpp> // On inclut la fonction à exposer
+#include <geomath/Vector.hpp>
+
+// --- INCLUSION MANQUANTE À AJOUTER ---
+#include <sstream> // Pour std::stringstream
 
 namespace py = pybind11;
 
-// PYBIND11_MODULE(nom_du_module_en_python, handle_du_module)
 PYBIND11_MODULE(geomath_py, m) {
     m.doc() = "Bindings Python pour la bibliothèque C++ GeoMatrix";
 
-    // On expose notre fonction C++ 'get_hello_message' sous le nom 'hello' en Python
-    m.def("hello", &geomath::get_hello_message, "Une fonction de test qui retourne un message");
+    py::class_<geomath::Vector<double, 3>>(m, "Vector3d")
+        .def(py::init<>())
+        .def("__repr__",
+            // Cette lambda capture une référence vers notre vecteur
+            [](const geomath::Vector<double, 3> &v) {
+                // 'ss' est maintenant un type connu
+                std::stringstream ss;
+                // Le compilateur trouvera maintenant geomath::operator<<
+                ss << v; 
+                return ss.str();
+            }
+        );
 }
